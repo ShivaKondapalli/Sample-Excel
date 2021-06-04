aa71b3d79132bd8b415f632b07d1af4d2f29ca37
Shiva Kondapalli <1993.ksk@gmail.com>
2021-06-04 10:43:39 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# import math
+# import os
+# import sys
+
+# import requests
+
+# print("version", sys.version)
+# print("executable", sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = f"Hello {who_to_greet}"
+#     return greeting
+
+
+# r = requests.get("https://en.wikipedia.org/wiki/Period_(algebraic_geometry)")
+# print(r.status_code)
+
+name = input("Enter your name:")
+print(f"Hi {name}!")
