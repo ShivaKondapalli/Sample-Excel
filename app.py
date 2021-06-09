5e3c3296250880510c00bdba589fa66c8c51a589
Shiva Kondapalli <1993.ksk@gmail.com>
2021-06-09 15:16:28 +0530

    reduced price by 10%

--- app.py
+++ app.py
@@ -2,6 +2,24 @@ import openpyxl as xl
 
 wb = xl.load_workbook("transactions.xlsx")
 sheet = wb["Sheet1"]
-cell = sheet["a"]
-for item in cell:
-    print(item)
+col_c = sheet["c"]  # cell = sheet["c1"] will give just a cell,
+
+corrected_price_lst = ["corrected_price"]
+
+for row in col_c:
+    if isinstance(row.value, float):
+        corrected_price = row.value * 0.9
+        corrected_price_lst.append(corrected_price)
+
+print(corrected_price_lst)
+
+col_d = sheet["d"]
+
+for value, row in zip(corrected_price_lst, col_d):
+    row.value = value
+
+print("=" * 10)
+for row in col_d:
+    print(row.value)
+
+wb.save("transactions2.xlsx")
